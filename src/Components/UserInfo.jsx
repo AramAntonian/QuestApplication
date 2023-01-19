17802fe53983696bbbbf04f4bf44c7e2b4f54898
GenaCollection <gena.emiryan91@gmail.com>
2023-01-20 01:43:01 +0400

    QuestPage

--- src/Components/UserInfo.jsx
+++ src/Components/UserInfo.jsx
@@ -14,23 +14,53 @@ function UserInfo({ userName }) {
           <th>
             <Link to="/firstMuseum">Museum 1</Link>
           </th>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.firstMuseum.lvl1
+                ? "green"
+                : "red",
+            }}
+          >
             Level 1
             {userName.Levels.firstMuseum.lvl1 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.firstMuseum.lvl2
+                ? "green"
+                : "red",
+            }}
+          >
             Level 2
             {userName.Levels.firstMuseum.lvl2 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.firstMuseum.lvl3
+                ? "green"
+                : "red",
+            }}
+          >
             Level 3
             {userName.Levels.firstMuseum.lvl3 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.firstMuseum.lvl4
+                ? "green"
+                : "red",
+            }}
+          >
             Level 4
             {userName.Levels.firstMuseum.lvl4 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.firstMuseum.lvl5
+                ? "green"
+                : "red",
+            }}
+          >
             Level 5
             {userName.Levels.firstMuseum.lvl5 ? ` Passed` : ` Not passed`}
           </td>
@@ -39,23 +69,53 @@ function UserInfo({ userName }) {
           <th>
             <Link to="/secondMuseum">Museum 2</Link>
           </th>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.secondMuseum.lvl1
+                ? "green"
+                : "red",
+            }}
+          >
             Level 1
             {userName.Levels.secondMuseum.lvl1 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.secondMuseum.lvl2
+                ? "green"
+                : "red",
+            }}
+          >
             Level 2
             {userName.Levels.secondMuseum.lvl2 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.secondMuseum.lvl3
+                ? "green"
+                : "red",
+            }}
+          >
             Level 3
             {userName.Levels.secondMuseum.lvl3 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.secondMuseum.lvl4
+                ? "green"
+                : "red",
+            }}
+          >
             Level 4
             {userName.Levels.secondMuseum.lvl4 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.secondMuseum.lvl5
+                ? "green"
+                : "red",
+            }}
+          >
             Level 5
             {userName.Levels.secondMuseum.lvl5 ? ` Passed` : ` Not passed`}
           </td>
@@ -64,23 +124,53 @@ function UserInfo({ userName }) {
           <th>
             <Link to="/thirdMuseum">Museum 3</Link>
           </th>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.thirdMuseum.lvl1
+                ? "green"
+                : "red",
+            }}
+          >
             Level 1
             {userName.Levels.thirdMuseum.lvl1 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.thirdMuseum.lvl2
+                ? "green"
+                : "red",
+            }}
+          >
             Level 2
             {userName.Levels.thirdMuseum.lvl2 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.thirdMuseum.lvl3
+                ? "green"
+                : "red",
+            }}
+          >
             Level 3
             {userName.Levels.thirdMuseum.lvl3 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.thirdMuseum.lvl4
+                ? "green"
+                : "red",
+            }}
+          >
             Level 4
             {userName.Levels.thirdMuseum.lvl4 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.thirdMuseum.lvl5
+                ? "green"
+                : "red",
+            }}
+          >
             Level 5
             {userName.Levels.thirdMuseum.lvl5 ? ` Passed` : ` Not passed`}
           </td>
@@ -89,23 +179,53 @@ function UserInfo({ userName }) {
           <th>
             <Link to="/forthMuseum">Museum 4</Link>
           </th>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.forthMuseum.lvl1
+                ? "green"
+                : "red",
+            }}
+          >
             Level 1
             {userName.Levels.forthMuseum.lvl1 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.forthMuseum.lvl2
+                ? "green"
+                : "red",
+            }}
+          >
             Level 2
             {userName.Levels.forthMuseum.lvl2 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.forthMuseum.lvl3
+                ? "green"
+                : "red",
+            }}
+          >
             Level 3
             {userName.Levels.forthMuseum.lvl3 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.forthMuseum.lvl4
+                ? "green"
+                : "red",
+            }}
+          >
             Level 4
             {userName.Levels.forthMuseum.lvl4 ? ` Passed` : ` Not passed`}
           </td>
-          <td className="status">
+          <td
+            style={{
+              "background-color": userName.Levels.forthMuseum.lvl5
+                ? "green"
+                : "red",
+            }}
+          >
             Level 5
             {userName.Levels.forthMuseum.lvl5 ? ` Passed` : ` Not passed`}
           </td>
