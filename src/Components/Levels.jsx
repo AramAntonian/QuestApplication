bcbad18d356a18ddf09e513e40219cf52bd300d8
AramAntonian <arama7014@gmail.com>
2023-01-23 04:07:26 -0800

    rewriting routes

--- src/Components/Levels.jsx
+++ src/Components/Levels.jsx
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 import "../style/Levels.css";
 
 function Levels({ num, userName }) {
-  const [latitude,setLatitude] = useState
-  const [longitude,setLongitude] = useState
+  const [latitude,setLatitude] = useState("")
+  const [longitude,setLongitude] = useState("")
 
   navigator.geolocation.getCurrentPosition(showPosition);
   function showPosition(position) {
     setLatitude(position.coords.latitude);
     setLongitude(position.coords.longitude);
   }
-  // const [answr,setAnswr] = useState 
+  // const [answr,setAnswr] = useState("") 
 console.log(latitude,longitude)
 
 //  const handleInput = event =>{
@@ -24,7 +24,7 @@ console.log(latitude,longitude)
     return (
       <div id="question">
         <button>
-          <Link to={`/${userName}/CharentsMuseum`}>Back to levels</Link>
+          <Link to={`/${userName.firstName}/CharentsMuseum`}>Back to levels</Link>
         </button>
         <h1>1</h1>
         <h2>When was the Yeghishe Charents House-Museum founded?</h2>
@@ -39,7 +39,7 @@ console.log(latitude,longitude)
         {/* <p>1964</p> */}
         {/* <h1>Hint</h1> */}
         <button>
-          <Link to={`/${userName}/CharentsMuseum/secondLevel`}>Next Level</Link>
+          <Link to={`/${userName.firstName}/CharentsMuseum/secondLevel`}>Next Level</Link>
         </button>
       </div>
     );
@@ -64,35 +64,35 @@ console.log(latitude,longitude)
   if (num === "1.3")
     return (
       <div>
-        <Link to={`/${userName}/CharentsMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum`}>Back to levels</Link>
         <h1>3</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/CharentsMuseum/forthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum/forthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/CharentsMuseum/secondLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum/secondLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "1.4")
     return (
       <div>
-        <Link to={`/${userName}/CharentsMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum`}>Back to levels</Link>
         <h1>4</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/CharentsMuseum/fifthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum/fifthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/CharentsMuseum/thirdLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum/thirdLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "1.5")
     return (
       <div>
-        <Link to={`/${userName}/CharentsMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum`}>Back to levels</Link>
         <h1>5</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
@@ -100,68 +100,68 @@ console.log(latitude,longitude)
         <h1>Hint</h1>
         <p>Next Level</p>
         <br />
-        <Link to={`/${userName}/CharentsMuseum/forthLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/CharentsMuseum/forthLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "2.1")
     return (
       <div>
-        <Link to={`/${userName}/TumanyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum`}>Back to levels</Link>
         <h1>1</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/TumanyanMuseum/secondLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/secondLevel`}>Next Level</Link>
         <p>Previous Level</p>
       </div>
     );
   if (num === "2.2")
     return (
       <div>
-        <Link to={`/${userName}/TumanyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum`}>Back to levels</Link>
         <h1>2</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/TumanyanMuseum/thirdLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/thirdLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/TumanyanMuseum/firstLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/firstLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "2.3")
     return (
       <div>
-        <Link to={`/${userName}/TumanyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum`}>Back to levels</Link>
         <h1>3</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/TumanyanMuseum/forthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/forthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/TumanyanMuseum/secondLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/secondLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "2.4")
     return (
       <div>
-        <Link to={`/${userName}/TumanyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum`}>Back to levels</Link>
         <h1>4</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/TumanyanMuseum/fifthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/fifthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/TumanyanMuseum/thirdLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/thirdLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "2.5")
     return (
       <div>
-        <Link to={`/${userName}/TumanyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum`}>Back to levels</Link>
         <h1>5</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
@@ -169,68 +169,68 @@ console.log(latitude,longitude)
         <h1>Hint</h1>
         <p>Next Level</p>
         <br />
-        <Link to={`/${userName}/TumanyanMuseum/forthLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/TumanyanMuseum/forthLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "3.1")
     return (
       <div>
-        <Link to={`/${userName}/IsahakyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum`}>Back to levels</Link>
         <h1>1</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/IsahakyanMuseum/secondLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/secondLevel`}>Next Level</Link>
         <p>Previous Level</p>
       </div>
     );
   if (num === "3.2")
     return (
       <div>
-        <Link to={`/${userName}/IsahakyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum`}>Back to levels</Link>
         <h1>2</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/IsahakyanMuseum/thirdLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/thirdLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/IsahakyanMuseum/firstLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/firstLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "3.3")
     return (
       <div>
-        <Link to={`/${userName}/IsahakyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum`}>Back to levels</Link>
         <h1>3</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/IsahakyanMuseum/forthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/forthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/IsahakyanMuseum/secondLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/secondLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "3.4")
     return (
       <div>
-        <Link to={`/${userName}/IsahakyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum`}>Back to levels</Link>
         <h1>4</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/IsahakyanMuseum/fifthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/fifthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/IsahakyanMuseum/thirdLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/thirdLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "3.5")
     return (
       <div>
-        <Link to={`/${userName}/IsahakyanMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum`}>Back to levels</Link>
         <h1>5</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
@@ -238,14 +238,14 @@ console.log(latitude,longitude)
         <h1>Hint</h1>
         <p>Next Level</p>
         <br />
-        <Link to={`/${userName}/IsahakyanMuseum/forthLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/IsahakyanMuseum/forthLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "4.1")
     return (
       <div id="question">
         <button>
-          <Link to={`/${userName}/KomitasMuseum`}>Back to levels</Link>
+          <Link to={`/${userName.firstName}/KomitasMuseum`}>Back to levels</Link>
         </button>
         <h1>1</h1>
         <h2>When was the Toumanyan House-Museum founded?</h2>
@@ -254,56 +254,56 @@ console.log(latitude,longitude)
         {/* <p>1964</p> */}
         {/* <h1>Hint</h1> */}
         <button>
-          <Link to={`/${userName}/KomitasMuseum/secondLevel`}>Next Level</Link>
+          <Link to={`/${userName.firstName}/KomitasMuseum/secondLevel`}>Next Level</Link>
         </button>
       </div>
     );
   if (num === "4.2")
     return (
       <div>
-        <Link to={`/${userName}/KomitasMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum`}>Back to levels</Link>
         <h1>2</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/KomitasMuseum/thirdLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum/thirdLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/KomitasMuseum/firstLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum/firstLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "4.3")
     return (
       <div>
-        <Link to={`/${userName}/KomitasMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum`}>Back to levels</Link>
         <h1>3</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/KomitasMuseum/forthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum/forthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/KomitasMuseum/secondLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum/secondLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "4.4")
     return (
       <div>
-        <Link to={`/${userName}/KomitasMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum`}>Back to levels</Link>
         <h1>4</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
         <p>answer</p>
         <h1>Hint</h1>
-        <Link to={`/${userName}/KomitasMuseum/fifthLevel`}>Next Level</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum/fifthLevel`}>Next Level</Link>
         <br />
-        <Link to={`/${userName}/KomitasMuseum/thirdLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum/thirdLevel`}>Previous Level</Link>
       </div>
     );
   if (num === "4.5")
     return (
       <div>
-        <Link to={`/${userName}/KomitasMuseum`}>Back to levels</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum`}>Back to levels</Link>
         <h1>5</h1>
         <h1>get question from data</h1>
         <h1>Answers</h1>
@@ -311,7 +311,7 @@ console.log(latitude,longitude)
         <h1>Hint</h1>
         <p>Next Level</p>
         <br />
-        <Link to={`/${userName}/KomitasMuseum/forthLevel`}>Previous Level</Link>
+        <Link to={`/${userName.firstName}/KomitasMuseum/forthLevel`}>Previous Level</Link>
       </div>
     );
 }
--- src/Components/LoggedInPage.jsx
+++ src/Components/LoggedInPage.jsx
@@ -9,10 +9,10 @@ function LoggedInPage({userName}){
     return (
         <div>
             <h1>some text</h1>
-            <Link to = {`/${userName.firstName}/firstMuseum`}><img src = "" alt = "1st museum" /></Link>
-            <Link to = {`/${userName.firstName}/secondMuseum`}><img src = "" alt = "2nd museum" /></Link>
-            <Link to = {`/${userName.firstName}/thirdMuseum`}><img src = "" alt = "3rd museum" /></Link>
-            <Link to = {`/${userName.firstName}/forthMuseum`}><img src = "" alt = "4th museum" /></Link>
+            <Link to = {`/${userName.firstName}/CharentsMuseum`}><img src = "" alt = "1st museum" /></Link>
+            <Link to = {`/${userName.firstName}/TumanyanMuseum`}><img src = "" alt = "2nd museum" /></Link>
+            <Link to = {`/${userName.firstName}/IsahakyanMuseum`}><img src = "" alt = "3rd museum" /></Link>
+            <Link to = {`/${userName.firstName}/KomitasMuseum`}><img src = "" alt = "4th museum" /></Link>
         </div>
     )
 }
