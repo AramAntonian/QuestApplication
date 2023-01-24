85b210faa175c963387896d78c1351f3f56e1b82
GenaCollection <gena.emiryan91@gmail.com>
2023-01-24 23:29:28 +0400

    QuestPage done

--- src/Components/QuestPage.jsx
+++ src/Components/QuestPage.jsx
@@ -1,279 +1,381 @@
+import { ArrowBackIos } from "@mui/icons-material";
+import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import "../style/QuestPage.css";
 
 function QuestPage({ num, userName }) {
   if (num === "1") {
     return (
-      <div id="list">
+      <div className="list">
         <Link to={`/${userName.firstName}`}>
-          <button>Back to Museums</button>{" "}
+          <ArrowBackIos />
         </Link>
         <br />
         <Link to={`/${userName.firstName}/CharentsMuseum/firstLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.firstMuseum.lvl1
-                ? "green"
-                : "red",
-            }}
-          >
-            1
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.firstMuseum.lvl1
+                  ? "green"
+                  : "red",
+              }}
+            >
+              1
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/CharentsMuseum/secondLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.firstMuseum.lvl2
-                ? "green"
-                : "red",
-            }}
-          >
-            2
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.firstMuseum.lvl2
+                  ? "green"
+                  : "red",
+              }}
+            >
+              2
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/CharentsMuseum/thirdLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.firstMuseum.lvl3
-                ? "green"
-                : "red",
-            }}
-          >
-            3
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.firstMuseum.lvl3
+                  ? "green"
+                  : "red",
+              }}
+            >
+              3
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/CharentsMuseum/forthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.firstMuseum.lvl4
-                ? "green"
-                : "red",
-            }}
-          >
-            4
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.firstMuseum.lvl4
+                  ? "green"
+                  : "red",
+              }}
+            >
+              4
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/CharentsMuseum/fifthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.firstMuseum.lvl5
-                ? "green"
-                : "red",
-            }}
-          >
-            5
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.firstMuseum.lvl5
+                  ? "green"
+                  : "red",
+              }}
+            >
+              5
+            </Button>
+          </div>
         </Link>
       </div>
     );
   }
   if (num === "2") {
     return (
-      <div id="list">
+      <div className="list">
         <Link to={`/${userName.firstName}`}>
-          <button>Back to Museums</button>{" "}
+          <ArrowBackIos />
         </Link>
         <br />
         <Link to={`/${userName.firstName}/TumanyanMuseum/firstLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.secondMuseum.lvl1
-                ? "green"
-                : "red",
-            }}
-          >
-            1
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.secondMuseum.lvl1
+                  ? "green"
+                  : "red",
+              }}
+            >
+              1
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/TumanyanMuseum/secondLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.secondMuseum.lvl2
-                ? "green"
-                : "red",
-            }}
-          >
-            2
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.secondMuseum.lvl2
+                  ? "green"
+                  : "red",
+              }}
+            >
+              2
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/TumanyanMuseum/thirdLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.secondMuseum.lvl3
-                ? "green"
-                : "red",
-            }}
-          >
-            3
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.secondMuseum.lvl3
+                  ? "green"
+                  : "red",
+              }}
+            >
+              3
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/TumanyanMuseum/forthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.secondMuseum.lvl4
-                ? "green"
-                : "red",
-            }}
-          >
-            4
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.secondMuseum.lvl4
+                  ? "green"
+                  : "red",
+              }}
+            >
+              4
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/TumanyanMuseum/fifthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.secondMuseum.lvl5
-                ? "green"
-                : "red",
-            }}
-          >
-            5
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.secondMuseum.lvl5
+                  ? "green"
+                  : "red",
+              }}
+            >
+              5
+            </Button>
+          </div>
         </Link>
       </div>
     );
   }
   if (num === "3") {
     return (
-      <div id="list">
+      <div className="list">
         <Link to={`/${userName.firstName}`}>
-          <button>Back to Museums</button>{" "}
+          <ArrowBackIos />
         </Link>
         <br />
         <Link to={`/${userName.firstName}/IsahakyanMuseum/firstLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.thirdMuseum.lvl1
-                ? "green"
-                : "red",
-            }}
-          >
-            1
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.thirdMuseum.lvl1
+                  ? "green"
+                  : "red",
+              }}
+            >
+              1
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/IsahakyanMuseum/secondLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.thirdMuseum.lvl2
-                ? "green"
-                : "red",
-            }}
-          >
-            2
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.thirdMuseum.lvl2
+                  ? "green"
+                  : "red",
+              }}
+            >
+              2
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/IsahakyanMuseum/thirdLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.thirdMuseum.lvl3
-                ? "green"
-                : "red",
-            }}
-          >
-            3
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.thirdMuseum.lvl3
+                  ? "green"
+                  : "red",
+              }}
+            >
+              3
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/IsahakyanMuseum/forthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.thirdMuseum.lvl4
-                ? "green"
-                : "red",
-            }}
-          >
-            4
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.thirdMuseum.lvl4
+                  ? "green"
+                  : "red",
+              }}
+            >
+              4
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/IsahakyanMuseum/fifthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.thirdMuseum.lvl5
-                ? "green"
-                : "red",
-            }}
-          >
-            5
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.thirdMuseum.lvl5
+                  ? "green"
+                  : "red",
+              }}
+            >
+              5
+            </Button>
+          </div>
         </Link>
       </div>
     );
   }
   if (num === "4") {
     return (
-      <div id="list">
+      <div className="list">
         <Link to={`/${userName.firstName}`}>
-          <button>Back to Museums</button>{" "}
+          <ArrowBackIos />
         </Link>
         <br />
         <Link to={`/${userName.firstName}/KomitasMuseum/firstLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.forthMuseum.lvl1
-                ? "green"
-                : "red",
-            }}
-          >
-            1
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.forthMuseum.lvl1
+                  ? "green"
+                  : "red",
+              }}
+            >
+              1
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/KomitasMuseum/secondLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.forthMuseum.lvl2
-                ? "green"
-                : "red",
-            }}
-          >
-            2
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.forthMuseum.lvl2
+                  ? "green"
+                  : "red",
+              }}
+            >
+              2
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/KomitasMuseum/thirdLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.forthMuseum.lvl3
-                ? "green"
-                : "red",
-            }}
-          >
-            3
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.forthMuseum.lvl3
+                  ? "green"
+                  : "red",
+              }}
+            >
+              3
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/KomitasMuseum/forthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.forthMuseum.lvl4
-                ? "green"
-                : "red",
-            }}
-          >
-            4
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.forthMuseum.lvl4
+                  ? "green"
+                  : "red",
+              }}
+            >
+              4
+            </Button>
+          </div>
         </Link>
         <br />
         <Link to={`/${userName.firstName}/KomitasMuseum/fifthLevel`}>
-          <button
-            style={{
-              "backgroundColor": userName.Levels.forthMuseum.lvl5
-                ? "green"
-                : "red",
-            }}
-          >
-            5
-          </button>
+          <div className="btn-container">
+            <Button
+              size="large"
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              style={{
+                backgroundColor: userName.Levels.forthMuseum.lvl5
+                  ? "green"
+                  : "red",
+              }}
+            >
+              5
+            </Button>
+          </div>
         </Link>
       </div>
     );
